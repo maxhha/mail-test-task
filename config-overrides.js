9116e149c24c0963d61903f1cae6d557ee3de323
maxhha <devmaxhha@gmail.com>
2022-05-19 11:27:22 +0300

    Serve staticfiles

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   paths: function (paths, env) {
+    paths.appBuild = path.resolve(__dirname, "build");
     paths.appPublic = path.resolve(__dirname, "frontend/public");
     paths.appHtml = path.resolve(__dirname, "frontend/public/index.html");
     paths.appIndexJs = path.resolve(__dirname, "frontend/src/index.js");
