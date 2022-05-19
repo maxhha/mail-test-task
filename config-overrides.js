7731e260dbca93b7e8b1d24be8c5217cd4299d15
maxhha <devmaxhha@gmail.com>
2022-05-19 12:24:00 +0300

    Split build folder to frontend and backend

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   paths: function (paths, env) {
-    paths.appBuild = path.resolve(__dirname, "build");
+    paths.appBuild = path.resolve(__dirname, "build/frontend_static");
     paths.appPublic = path.resolve(__dirname, "frontend/public");
     paths.appHtml = path.resolve(__dirname, "frontend/public/index.html");
     paths.appIndexJs = path.resolve(__dirname, "frontend/src/index.js");
