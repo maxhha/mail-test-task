1011d7ef1922b3956929cab13856de7994be982b
maxhha <devmaxhha@gmail.com>
2022-05-19 12:29:04 +0300

    Change build dir names

--- mailtesttask/settings.py
+++ mailtesttask/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'build/backend_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build/backend/static')
 STATIC_URL = '/static/'
 # MEDIA_URL = '/uploaded/'
 
